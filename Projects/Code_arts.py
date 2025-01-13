032acdf36f1e6abcf5436afe4f6230b5604ebb71
miles-yn <milesshahine@seattleacademy.org>
2025-01-13 16:35:03 +0000

    Added color

--- Projects/Code_arts.py
+++ Projects/Code_arts.py
@@ -1,15 +1,19 @@
 import turtle
 import random
+#Adds random function
 
 t = turtle.Turtle()
 
-t.color ('purple')
 t.speed(10)
 
 angles = [0, 60, 120, 180, 240, 300]
+#giving angles to choose from
+colors = ["black", "green", "purple"]
 for i in range (1000):
     t.forward (10)
     t.left (random.choice(angles))
-    
+#usage of random
+    t.color ( colors[i % 3] )
+
 
 turtle.exitonclick()
