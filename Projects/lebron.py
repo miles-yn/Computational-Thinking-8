b1c140eaf489e0a57901ce1ecac55a77197f606e
miles-yn <milesshahine@seattleacademy.org>
2025-02-04 22:49:38 +0000

    Attempted to add sprites and imported codesters

---
+++ Projects/lebron.py
@@ -0,0 +1,7 @@
+import codesters, random
+from codesters import StageClass
+stage = StageClass()
+stage.disable_floor()
+
+codesters.sprite = ("/workspaces/Computational-Thinking-8/Backgrounds/legreatnessdriveway.png")
+
