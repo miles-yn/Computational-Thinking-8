a764a730773524fbebe3e05ff1ac8fd0f2f1c5c6
miles-yn <milesshahine@seattleacademy.org>
2025-02-24 19:57:32 +0000

    LeMiles_LeKayden

--- Projects/lebron.py
+++ Projects/lebron.py
@@ -3,5 +3,16 @@ from codesters import StageClass
 stage = StageClass()
 stage.disable_floor()
 
-codesters.sprite = ("/workspaces/Computational-Thinking-8/Backgrounds/legreatnessdriveway.png")
+stage.set_background("cool_dog.png")
 
+def falling_object():
+    global object_speed, lives
+
+    if lives > 0:
+        x_position = random.randint(-250,250)
+        object = codesters.Sprite("P.png")
+        object.set_size(0.4)
+        object.set_y_speed(object_speed)
+        object_speed = 0.5
+
+stage.event_interval(falling_object, 0.5)
