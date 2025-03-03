79e8abdd7c0e4554a00e3a8e07d373996de86df2
miles-yn <milesshahine@seattleacademy.org>
2025-03-03 23:54:16 +0000

    Setup and Controls Submission

--- Projects/lebron.py
+++ Projects/lebron.py
@@ -1,18 +1,28 @@
+#Section 1 - Setup
 import codesters, random
 from codesters import StageClass
 stage = StageClass()
 stage.disable_floor()
 
-stage.set_background("cool_dog.png")
+object = codesters.Sprite("lehairline.png")
+object.set_width(100)
+object.set_height(100)
+stage.set_background("legreatnessdriveway.png")
 
-def falling_object():
-    global object_speed, lives
-
-    if lives > 0:
-        x_position = random.randint(-250,250)
-        object = codesters.Sprite("P.png")
-        object.set_size(0.4)
-        object.set_y_speed(object_speed)
-        object_speed = 0.5
-
-stage.event_interval(falling_object, 0.5)
+# Section 4 - Controls
+def move_up(sprite):
+	sprite.move_up(1)
+   	 
+def move_down(sprite):
+	sprite.move_down(1)
+    
+def move_left(sprite):
+	sprite.move_left(1)
+    
+def move_right(sprite):    
+	sprite.move_right(1)
+	
+object.event_key("up", move_up)
+object.event_key("down", move_down)
+object.event_key("left", move_left)
+object.event_key("right", move_right)
